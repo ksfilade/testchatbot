126ccc5df4a1ab583fa2af4658607fe4c934de06
kiril <ksfilade@gmail.com>
2021-09-11 14:54:03 +0200

    fixed start

--- src/controllers/chatfacebookController.js
+++ src/controllers/chatfacebookController.js
@@ -70,6 +70,7 @@ function handleMessage(sender_psid, received_message) {
         "text": `You sent the message: "${received_message.text}". Now send me an image!`
       }
     }  
+    console.log(response)
     
     // Sends the response message
     callSendAPI(sender_psid, response);    
@@ -81,12 +82,25 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
+    console.log('here')
     let request_body = {
         "recipient": {
           "id": sender_psid
         },
         "message": response
       }
+      request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": process.env.FACEBOOK_PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+      }, (err, res, body) => {
+        if (!err) {
+          console.log('message sent!')
+        } else {
+          console.error("Unable to send message:" + err);
+        }
+      }); 
 }
 module.exports = {
     postWebhook: postWebhook,
