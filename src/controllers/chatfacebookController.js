6d700ec33fbe496571ae14d3f7ffd9ae8e65eace
kiril <ksfilade@gmail.com>
2021-09-11 14:50:42 +0200

    fixed start

--- src/controllers/chatfacebookController.js
+++ src/controllers/chatfacebookController.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const request = require('request');
 
 let postWebhook = (req, res) =>{
     // Parse the request body from the POST
@@ -14,6 +15,15 @@ let postWebhook = (req, res) =>{
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+      let sender_psid = webhook_event.sender.id;
+         console.log('Sender PSID: ' + sender_psid);
+    // Check if the event is a message or postback and
+  // pass the event to the appropriate handler function
+        if (webhook_event.message) {
+            handleMessage(sender_psid, webhook_event.message);        
+        } else if (webhook_event.postback) {
+            handlePostback(sender_psid, webhook_event.postback);
+        }
     });
 
     // Returns a '200 OK' response to all requests
@@ -48,6 +58,36 @@ let getWebhook = (req, res) => {
     }
   }
 };
+function handleMessage(sender_psid, received_message) {
+
+    let response;
+  
+    // Check if the message contains text
+    if (received_message.text) {    
+  
+      // Create the payload for a basic text message
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      }
+    }  
+    
+    // Sends the response message
+    callSendAPI(sender_psid, response);    
+  }
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    let request_body = {
+        "recipient": {
+          "id": sender_psid
+        },
+        "message": response
+      }
+}
 module.exports = {
     postWebhook: postWebhook,
     getWebhook: getWebhook
